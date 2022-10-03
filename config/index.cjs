6c1ea49ac407ecacf171558901b398a65fcc1aba
Eugene_liao <71582155+2664375039@users.noreply.github.com>
2022-10-03 13:03:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'e47aacba8d91308b59034bf10f883ee2',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'Fq78spFqAeGtfN2FDw72_k1a2y6qfMfUkfp_092PK8o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-11',
       festivals: [
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '婷婷', year: '2001', date: '02-22',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
